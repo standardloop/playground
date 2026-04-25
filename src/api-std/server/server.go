5cbc544608caef9260b6ee6fc377e2727bffea5d
standardloop <joshuaeli1234@gmail.com>
2026-04-25 22:13:57 +0800

    reorder

--- src/api-std/server/server.go
+++ src/api-std/server/server.go
@@ -14,7 +14,7 @@ func loggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 		next.ServeHTTP(w, r)
-		slog.Info(r.URL.Path)
+		slog.Info(fmt.Sprintf("%s %s", r.Method, r.URL.Path))
 	})
 }
 
@@ -22,6 +22,7 @@ func Init() {
 	mainMux := http.NewServeMux()
 	apiMux := http.NewServeMux()
 
+	// API
 	apiMux.HandleFunc("/", http.HandlerFunc(handlers.GenericNotFoundHandler))
 	apiMux.Handle("GET /env", loggingMiddleware(http.HandlerFunc(v1.EnvHandler)))
 	apiMux.Handle("GET /healthz", loggingMiddleware(http.HandlerFunc(health.BasicHealthHandler)))
@@ -49,14 +50,17 @@ func Init() {
 	// todo
 	apiMux.Handle("GET /health/intergrations", loggingMiddleware(http.HandlerFunc(health.BasicHealthHandler)))
 
+	apiMux.Handle("GET /headers", loggingMiddleware(http.HandlerFunc(v1.HeadersHandler)))
+	apiMux.Handle("GET /crash", loggingMiddleware(http.HandlerFunc(v1.CrashHandler)))
+	apiMux.Handle("GET /crash/{code}", loggingMiddleware(http.HandlerFunc(v1.CrashCodeHandler)))
+	apiMux.Handle("GET /status/{code}", loggingMiddleware(http.HandlerFunc(v1.StatusHandler)))
+	apiMux.Handle("GET /delay/{seconds}", loggingMiddleware(http.HandlerFunc(v1.DelayHandler)))
+	apiMux.Handle("POST /echo", loggingMiddleware(http.HandlerFunc(v1.EchoHandler)))
+
 	mainMux.Handle(config.ApiVersion+"/", http.StripPrefix(config.ApiVersion, apiMux))
-	mainMux.HandleFunc("GET /headers", http.HandlerFunc(handlers.HeadersHandler))
-	mainMux.HandleFunc("GET /crash", http.HandlerFunc(handlers.CrashHandler))
-	mainMux.HandleFunc("GET /crash/{code}", http.HandlerFunc(handlers.CrashCodeHandler))
-	mainMux.HandleFunc("GET /status/{code}", http.HandlerFunc(handlers.StatusHandler))
-	mainMux.HandleFunc("GET /delay/{seconds}", http.HandlerFunc(handlers.DelayHandler))
-	mainMux.HandleFunc("POST /echo", http.HandlerFunc(handlers.EchoHandler))
-	mainMux.HandleFunc("/", http.HandlerFunc(handlers.GenericNotFoundHandler))
+
+	// GLOBAL
+	mainMux.Handle("/", loggingMiddleware(http.HandlerFunc(handlers.GenericNotFoundHandler)))
 
 	http.ListenAndServe(fmt.Sprintf(":%s", config.Env.AppPort), mainMux)
 }
