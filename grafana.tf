16735cd8b16b71578339e5fec6df5fd16bc1eb47
standardloop <joshuaeli1234@gmail.com>
2026-07-11 13:46:12 +0800

    grafana

--- grafana.tf
+++ grafana.tf
@@ -1,17 +1,59 @@
-data "kustomization_build" "metrics_server" {
-  path = "deploy/infrastructure/metrics-server"
+// operator
+data "kustomization_build" "grafana_operator" {
+  path = "deploy/infrastructure/grafana/operator"
 }
 
-resource "kustomization_resource" "metrics_server" {
-  for_each   = data.kustomization_build.metrics_server.ids
-  manifest   = data.kustomization_build.metrics_server.manifests[each.value]
+resource "kustomization_resource" "grafana_operator" {
+  for_each   = data.kustomization_build.grafana_operator.ids
+  manifest   = data.kustomization_build.grafana_operator.manifests[each.value]
   depends_on = [helm_release.flux2, kustomization_resource.flux_sources]
 }
 
-resource "null_resource" "wait_for_metrics_server" {
-  depends_on = [kustomization_resource.metrics_server]
+resource "null_resource" "wait_for_grafana_operator" {
+  depends_on = [kustomization_resource.grafana_operator]
 
   provisioner "local-exec" {
-    command = "kubectl wait helmrelease/metrics-server --for=condition=Ready --timeout=180s -n kube-system"
+    command = "kubectl wait helmrelease/grafana-operator --for=condition=Ready --timeout=180s -n grafana"
   }
+}
+
+// instance
+module "wait_for_crds_needed_for_grafana_instance" {
+    depends_on = [ null_resource.wait_for_grafana_operator ]
+  source = "./modules/wait-for-crd"
+  crds = [
+    "grafanas.grafana.integreatly.org",
+    "grafanadatasources.grafana.integreatly.org"
+  ]
+}
+
+data "kustomization_build" "grafana_instance" {
+  path = "deploy/infrastructure/grafana/instance"
+}
+
+resource "kustomization_resource" "grafana_instance" {
+  for_each   = data.kustomization_build.grafana_instance.ids
+  manifest   = data.kustomization_build.grafana_instance.manifests[each.value]
+  depends_on = [helm_release.flux2, kustomization_resource.flux_sources, null_resource.wait_for_grafana_operator, module.wait_for_crds_needed_for_grafana_instance]
+}
+
+// dashboards
+module "wait_for_crds_needed_for_grafana_dashboards" {
+    depends_on = [ null_resource.wait_for_grafana_operator ]
+  source = "./modules/wait-for-crd"
+  crds = [
+    "grafanadashboards.grafana.integreatly.org",
+  ]
+}
+
+resource "helm_release" "grafana_dashboards" {
+  depends_on = [module.wait_for_crds_needed_for_grafana_dashboards, kustomization_resource.grafana_instance]
+  name       = "grafana-dashboards"
+  chart      = "./deploy/infrastructure/grafana/dashboards"
+  namespace  = "grafana"
+
+  values = [
+    file("./deploy/infrastructure/grafana/dashboards/values.yaml"),
+    file("./deploy/infrastructure/grafana/dashboards/env/playground.values.yaml")
+  ]
 }
