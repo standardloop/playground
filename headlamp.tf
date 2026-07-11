ca29c2296e60d825b7c4805bb3e15987257f7bca
standardloop <joshuaeli1234@gmail.com>
2026-07-11 14:31:19 +0800

    不錯

--- headlamp.tf
+++ headlamp.tf
@@ -1,24 +1,24 @@
-/*
-    WIP
-    - kubectl -n kube-system create serviceaccount headlamp-admin  # wip
-    - kubectl create clusterrolebinding headlamp-playground-admin --serviceaccount=kube-system:headlamp-admin --clusterrole=cluster-admin
-    - kubectl create token headlamp-admin -n kube-system | pbcopy
-*/
+# /*
+#     WIP
+#     - kubectl -n kube-system create serviceaccount headlamp-admin  # wip
+#     - kubectl create clusterrolebinding headlamp-playground-admin --serviceaccount=kube-system:headlamp-admin --clusterrole=cluster-admin
+#     - kubectl create token headlamp-admin -n kube-system | pbcopy
+# */
 
-data "kustomization_build" "headlamp" {
-  path = "deploy/infrastructure/headlamp"
-}
+# data "kustomization_build" "headlamp" {
+#   path = "deploy/infrastructure/headlamp"
+# }
 
-resource "kustomization_resource" "headlamp" {
-  for_each   = data.kustomization_build.headlamp.ids
-  manifest   = data.kustomization_build.headlamp.manifests[each.value]
-  depends_on = [helm_release.flux2, kustomization_resource.flux_sources]
-}
+# resource "kustomization_resource" "headlamp" {
+#   for_each   = data.kustomization_build.headlamp.ids
+#   manifest   = data.kustomization_build.headlamp.manifests[each.value]
+#   depends_on = [helm_release.flux2, kustomization_resource.flux_sources]
+# }
 
-resource "null_resource" "wait_for_headlamp" {
-  depends_on = [kustomization_resource.headlamp]
+# resource "null_resource" "wait_for_headlamp" {
+#   depends_on = [kustomization_resource.headlamp]
 
-  provisioner "local-exec" {
-    command = "kubectl wait helmrelease/headlamp --for=condition=Ready --timeout=180s -n kube-system"
-  }
-}
+#   provisioner "local-exec" {
+#     command = "kubectl wait helmrelease/headlamp --for=condition=Ready --timeout=180s -n kube-system"
+#   }
+# }
